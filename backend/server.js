6104e9e43be70e66b3d4851f40694e28fb33cd2d
kunal <kunalsingh5431@gmail.com>
2024-12-14 13:15:48 +0530

    Backend-1

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,16 @@
 const express = require("express");
-const app =express();
+const dotenv=require('dotenv');
+const cors=require('cors');
+const bodyParser=require('body-parser');
 const port = 8000;
+
+dotenv.config();
+
+const app =express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
 
 app.get("/", (req, res) => {
     res.send("I am Root");
